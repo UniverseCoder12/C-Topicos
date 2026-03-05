9d501d244e76ac2b923e99a1d3e9a3f30fd318dc
UniverseCoder12 <166874928+UniverseCoder12@users.noreply.github.com>
2026-03-05 17:32:56 +0000

    popo

--- testes/helloworld.c
+++ testes/helloworld.c
@@ -1,9 +1,11 @@
 int main()
 {
-    int f = 0;
+    int f = 10;
     int y = 10000000;
+    int res = f+y;
 
     printf("Olá Mundo!\n");
-    
+    printf("f + Y: %d \n ", res);
+
     return 0;
 }
