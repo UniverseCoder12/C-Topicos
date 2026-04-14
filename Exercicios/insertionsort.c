9e3a5123c3e189b50a60fd0ca64c9acb959196d3
UniverseCoder12 <166874928+UniverseCoder12@users.noreply.github.com>
2026-04-14 19:23:19 +0000

    efef

--- Exercicios/insertionsort.c
+++ Exercicios/insertionsort.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 #define TAM 10
 
-int main()
-{
-    int nums[TAM];
-    int *ptr;
-    int aux;
-    for(ptr = &nums; ptr < nums+TAM; ptr++)
-    {
-        printf("digite o um número: ");
-        scanf("%d", ptr);
+int main() {
+  int nums[TAM];
+  int *ptr;
+  int aux;
+  for (ptr = nums; ptr < nums + TAM; ptr++) {
+    printf("digite o um número: ");
+    scanf("%d", ptr);
+  }
+
+  for (ptr = nums; ptr < nums + (TAM - 1); ptr++) {
+    for (int *ptr2 = ptr + 1; ptr2 > nums; ptr2--) {
+      int *ptrAnterior = ptr2 - 1;
+      if (*ptr2 < *ptrAnterior ) {
+        aux = *ptr2;
+        *ptr2 = *ptrAnterior;
+        *ptrAnterior = aux;
+      }
     }
+  }
 
+  for (ptr = &nums[0]; ptr < &nums[TAM]; ptr++) {
+    printf("%d\n", *ptr);
+  }
 
-
-    for(ptr = &nums; ptr < nums+TAM; ptr++)
-    {
-        for(int *ptr2 = ptr+1; ptr2 >= nums; ptr2--)
-        {
-            if (*ptr2 < *(ptr2+1)) {
-                aux = *ptr2;
-                *ptr2 = *(ptr2+1);
-                *(ptr2+1) = aux;
-            }
-            
-        }
-    }
-
-    for(ptr = &nums[0]; ptr < &nums[TAM]; ptr++)
-    {
-        printf("%d\n", *ptr);
-    }
-
-    return 0;
+  return 0;
 }
